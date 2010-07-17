7fc851e96b0537e4ac0ff6e920984f980d90d9ce
Diego Guimaraes <diegodukao@gmail.com>
2010-07-17 03:08:49 -0300

    Hello, PyGame!

---
+++ main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+import pygame
+
+screen = pygame.display.set_mode((640, 400))
+running = 1
+
+while running:
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running = 0
+    
+    screen.fill((0, 20, 0))
+    pygame.display.flip()
