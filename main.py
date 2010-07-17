528c953e619d8d5e8f89bc1f12a0e8773195c872
Diego Guimaraes <diegodukao@gmail.com>
2010-07-17 17:56:05 -0300

    the value_collected is increasing as the player throws the hook

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Game:
     
     def load_sprites(self):
         """Load the sprites that we need"""
-        self.player_boat = Boat()
+        self.player_boat = Boat([60, 300])
         self.player_boat_sprites = pygame.sprite.RenderPlain((self.player_boat))
     
     def main(self):
@@ -37,6 +37,9 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == KEYDOWN:
+                    if (event.key == K_RETURN):
+                        self.player_boat.throw_hook()
             
             self.screen.blit(self.bg, self.bg_rect)
             self.player_boat_sprites.draw(self.screen)
@@ -46,9 +49,26 @@ class Game:
 class Boat(pygame.sprite.Sprite):
     """The boats that will represent the player and the computer"""
     
-    def __init__(self):
+    def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image('paper_boat.jpg')
+        self.rect.topleft = position
+        self.value_collected = 0
+        
+    def throw_hook(self):
+        fish = Fish()
+        self.value_collected += fish.value
+        print(self.value_collected)
+
+
+class Fish(pygame.sprite.Sprite):
+    """The values that the player and the computer will fish"""
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('coin.png')
+        self.value = 10
+
 
 if __name__ == "__main__":
     game_window = Game()
