01d4b26fbb17e98a117ccd3c31cd4102f9976d16
Diego Guimaraes <diegodukao@gmail.com>
2010-07-17 16:44:04 -0300

    the first boat is appearing on the top of the screen

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import pygame
 from helpers import *
 
-class GameMain:
+class Game:
     """The Main Game Class - This class handles the main 
     initialization and creating of the Game."""
     
@@ -22,9 +22,16 @@ class GameMain:
         """Create the background"""
         self.bg, self.bg_rect = load_image("ocean.jpg")
         self.bg = self.bg.convert()
-
     
-    def MainLoop(self):
+    def load_sprites(self):
+        """Load the sprites that we need"""
+        self.player_boat = Boat()
+        self.player_boat_sprites = pygame.sprite.RenderPlain((self.player_boat))
+    
+    def main(self):
+        """Load all of our sprites"""
+        self.load_sprites()
+        
         """This is the Main Loop of the game"""
         while 1:
             for event in pygame.event.get():
@@ -32,10 +39,18 @@ class GameMain:
                     sys.exit()
             
             self.screen.blit(self.bg, self.bg_rect)
+            self.player_boat_sprites.draw(self.screen)
             pygame.display.flip()
 
 
+class Boat(pygame.sprite.Sprite):
+    """The boats that will represent the player and the computer"""
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('paper_boat.jpg')
+
 if __name__ == "__main__":
-    MainWindow = GameMain()
-    MainWindow.MainLoop()
+    game_window = Game()
+    game_window.main()
 
