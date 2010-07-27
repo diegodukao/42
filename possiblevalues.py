01a7f603766477468dd03d317b869ccc249a528d
Diego Guimaraes <diegodukao@gmail.com>
2010-07-27 13:14:28 -0300

    creating different files for each coin

--- possiblevalues.py
+++ possiblevalues.py
@@ -1,17 +1,17 @@
 class PossibleValues:
     def __init__(self):
         self.list = [
-            (4, "coin.png"), (4, "coin.png"), (4, "coin.png"), (4, "coin.png"),
-            (6, "coin.png"), (6, "coin.png"), (6, "coin.png"), (6, "coin.png"),
-            (8, "coin.png"), (8, "coin.png"), (8, "coin.png"), (8, "coin.png"),
-            (10, "coin.png"), (10, "coin.png"), (10, "coin.png"), (10, "coin.png"),
-            (12, "coin.png"), (12, "coin.png"), (12, "coin.png"), (12, "coin.png"),
-            (14, "coin.png"), (14, "coin.png"), (14, "coin.png"), (14, "coin.png"),
-            (16, "coin.png"), (16, "coin.png"), (16, "coin.png"), (16, "coin.png"),
-            (18, "coin.png"), (18, "coin.png"), (18, "coin.png"), (18, "coin.png"),
-            (20, "coin.png"), (20, "coin.png"), (20, "coin.png"), (20, "coin.png"),
-            (20, "coin.png"), (20, "coin.png"), (20, "coin.png"), (20, "coin.png"),
-            (20, "coin.png"), (20, "coin.png"), (20, "coin.png"), (20, "coin.png"),
-            (20, "coin.png"), (20, "coin.png"), (20, "coin.png"), (20, "coin.png"),
-            (21, "coin.png"), (21, "coin.png"), (21, "coin.png"), (21, "coin.png")
+            (4, "coin4.png"), (4, "coin4.png"), (4, "coin4.png"), (4, "coin4.png"),
+            (6, "coin6.png"), (6, "coin6.png"), (6, "coin6.png"), (6, "coin6.png"),
+            (8, "coin8.png"), (8, "coin8.png"), (8, "coin8.png"), (8, "coin8.png"),
+            (10, "coin10.png"), (10, "coin10.png"), (10, "coin10.png"), (10, "coin10.png"),
+            (12, "coin12.png"), (12, "coin12.png"), (12, "coin12.png"), (12, "coin12.png"),
+            (14, "coin14.png"), (14, "coin14.png"), (14, "coin14.png"), (14, "coin14.png"),
+            (16, "coin16.png"), (16, "coin16.png"), (16, "coin16.png"), (16, "coin16.png"),
+            (18, "coin18.png"), (18, "coin18.png"), (18, "coin18.png"), (18, "coin18.png"),
+            (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"),
+            (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"),
+            (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"),
+            (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"), (20, "coin20.png"),
+            (21, "coin21.png"), (21, "coin21.png"), (21, "coin21.png"), (21, "coin21.png")
         ]
